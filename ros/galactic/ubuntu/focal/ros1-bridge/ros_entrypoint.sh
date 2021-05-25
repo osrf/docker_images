ffa55d00247689c5ac410e45c38636e95ee77731
Keisuke Shima <19993104+KeisukeShima@users.noreply.github.com>
2021-05-25 18:10:28 +0900

    add galactic support

---
+++ ros/galactic/ubuntu/focal/ros1-bridge/ros_entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# unsetting ROS_DISTRO to silence ROS_DISTRO override warning
+unset ROS_DISTRO
+# setup ros1 environment
+source "/opt/ros/$ROS1_DISTRO/setup.bash"
+
+# unsetting ROS_DISTRO to silence ROS_DISTRO override warning
+unset ROS_DISTRO
+# setup ros2 environment
+source "/opt/ros/$ROS2_DISTRO/setup.bash"
+
+exec "$@"
